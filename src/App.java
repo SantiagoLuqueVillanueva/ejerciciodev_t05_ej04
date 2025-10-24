916a8a7743d7a8952e059079c501f3467d3ddee5
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-24 18:54:11 +0200

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int i = 320; (i <= 320) && (i >= 160); i -= 20){
+            System.out.print(i + " ");
+        }
     }
 }
